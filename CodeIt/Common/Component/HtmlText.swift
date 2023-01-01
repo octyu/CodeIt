7c2d44fa8dee803a66b8ac4ea6dd302e51a6618e
octyu <541004828@qq.com>
2023-01-01 23:51:59 +0800

    feat: add pre tag

--- CodeIt/Common/Component/HtmlText.swift
+++ CodeIt/Common/Component/HtmlText.swift
@@ -19,9 +19,10 @@ struct HTMLText: NSViewRepresentable {
         .font(.systemFont(ofSize: 12))
         .backgroundColor(NSColor(rgb: 0xf6f8fa))
     let strong = Style("strong").font(.boldSystemFont(ofSize: 16))
+    let pre = Style("pre").font(.systemFont(ofSize: 14)).backgroundColor(NSColor(rgb: 0xf6f8fa))
     
     func makeNSView(context: Context) -> NSTextField {
-        return NSTextField(labelWithAttributedString: text.style(tags: p, code, strong).attributedString)
+        return NSTextField(labelWithAttributedString: text.style(tags: p, code, strong, pre).attributedString)
     }
     func updateNSView(_ nsTextField: NSTextField, context: Context) {
         // implement protocol
